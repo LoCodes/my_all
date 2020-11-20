174bb34050a3b18ce01f03ee6995d4353b26afee
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-20 11:47:58 -0800

    commit tree 8315901dc0f1152e7281c851051f61447b7da459

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,23 @@
 require 'pry'
 
 def my_all?(collection)
+    i = 0
+    block_return_values = []
+    while i < collection.length
+      block_return_values << yield(collection[i])
+      i = i + 1 #i += 1 does the same thing. Use this if it's easier for you.
+    end
+  
+    if block_return_values.include?(false)
+      false
+    else
+      true
+    end
+  end 
 
-end
+#   #my_all?
+#   does not call on all?
+#   can handle an empty collection
+#   yields the correct element to the block
+#   returns false when the block condition is not met
+#   returns true when the block condition is met
